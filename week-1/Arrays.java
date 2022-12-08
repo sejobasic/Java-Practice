4139fb8de6105324649ed547515183286b6e0ecd
Sejo Basic <sejoob@gmail.com>
2022-12-08 11:59:21 -0700

    week-1 array workbook challenges completed

--- week-1/Arrays.java
+++ week-1/Arrays.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Arrays {
   public static void main(String[] args) {
-    String[] seats = {"Harry", "Neville", "Ron", "Hermione", "Seamus"};
+    Scanner scan = new Scanner(System.in);
+
+    String[] seats = { "Harry", "Neville", "Ron", "Hermione", "Seamus" };
 
     System.out.println(seats[0] + ", you will take seat 0");
     System.out.println(seats[1] + ", you will take seat 1");
@@ -8,10 +12,44 @@ public class Arrays {
     System.out.println(seats[3] + ", you will take seat 3");
     System.out.println(seats[4] + ", you will take seat 4");
 
-
-    String[] cars = {"E30", "E40", "E28"};
+    String[] cars = { "E30", "E40", "E28" };
     for (int i = 0; i < cars.length; i++) {
       System.out.println(cars[i]);
     }
+
+    String[] items = { "apples", "bananas", "candy", "chocolate", "coffee", "tea" };
+
+    System.out.println("Welcome to Java Grocers.");
+    System.out.println("What can I help you find?\n");
+    String userInput = scan.nextLine();
+
+    for (int i = 0; i < items.length; i++) {
+      if (items[i].equals(userInput)) {
+        System.out.println("\nWe have that in aisle: " + i);
+        break;
+      }
+    }
+
+    scan.close();
+
+    int[] scores = { getRandomNum(), getRandomNum(), getRandomNum(), getRandomNum() };
+    int highScore = 0;
+
+    System.out.print("Here are the scores: ");
+    for (int i = 0; i < scores.length; i++) {
+      System.out.print(scores[i] + " ");
+
+      if (scores[i] > highScore) {
+        highScore = scores[i];
+      }
+    }
+
+    System.out.println("\nThe highest score is: " + highScore);
+
+  }
+
+  public static int getRandomNum() {
+    double randomNum = Math.random() * 50000;
+    return (int) randomNum;
   }
 }
