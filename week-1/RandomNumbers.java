cac162c6790455906bfba42f1c5718cdc5d3a1a1
Sejo Basic <sejoob@gmail.com>
2022-12-08 16:09:59 -0700

    week-1 random numbers challenge completed

---
+++ week-1/RandomNumbers.java
@@ -0,0 +1,26 @@
+public class RandomNumbers {
+  public static void main(String[] args) {
+    int[][] array = new int[100][10];
+    
+    for (int i = 0; i < array.length; i++) {
+      for (int j = 0; j < array[i].length; j++) {
+        array[i][j] = randomNumber();
+      }
+    }
+
+    printArray(array);
+    
+  }
+  public static int randomNumber() {
+    double randomNumber = Math.random()*100;
+    return (int)randomNumber;
+  }
+  public static void printArray(int[][] array) {
+    for (int i = 0; i < array.length; i++) {
+      for (int j = 0; j < array[i].length; j++) {
+        System.out.print(array[i][j] + " ");
+      }
+      System.out.print("\n");
+    }
+  }
+}
