4e0001ec13f579ce999d47a07d1e0d81a824e2a7
Sejo Basic <sejoob@gmail.com>
2022-12-12 19:18:41 -0700

    Object constructor practice

--- week-2/ObjectsPractice/src/Main.java
+++ week-2/ObjectsPractice/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person mark = new Person("mark", "bosnian", "12/09/93", 29);
+
+        System.out.println("Name: " + mark.name + "\n" + "Nationality: " + mark.nationality + "\n" + "Date of Birth: " + mark.dateOfBirth + "\n" + "Seat Number: " + mark.seatNumber + "\n");
     }
 }
---
+++ week-2/ObjectsPractice/src/Person.java
@@ -0,0 +1,14 @@
+public class Person {
+    public String name;
+    public String nationality;
+    public String dateOfBirth;
+    int seatNumber;
+
+    public Person(String name, String nationality, String dateOfBirth, int seatNumber) {
+        this.name = name;
+        this.nationality = nationality;
+        this.dateOfBirth = dateOfBirth;
+        this.seatNumber = seatNumber;
+    }
+
+}
