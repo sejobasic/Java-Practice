9765ac705a04758b1beab80ecf60d288ef6baf28
Sejo Basic <sejoob@gmail.com>
2022-12-12 20:26:29 -0700

    Object getter and setter method introduction complete

--- week-2/ObjectsPractice/src/Main.java
+++ week-2/ObjectsPractice/src/Main.java
@@ -1,7 +1,23 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Person mark = new Person("mark", "bosnian", "12/09/93", 29);
 
-        System.out.println("Name: " + mark.getName() + "\n" + "Nationality: " + mark.getNationality() + "\n" + "Date of Birth: " + mark.getDateOfBirth() + "\n" + "Seat Number: " + mark.getSeatNumber() + "\n");
+        Person mark = new Person("mark", "bosnian", "12/09/93", new String[] {"Expires: 2030", "International"},35);
+
+        mark.setName("Mark Johnson");
+        mark.setPassport(new String[]{"Expires 2020", "Domestic"});
+        mark.chooseSeat();
+
+        System.out.println("Name: " + mark.getName() + "\nNationality: " + mark.getNationality() + "\nDate Of Birth: " + mark.getDateOfBirth() + "\nPassport: " + Arrays.toString(mark.getPassport()));
+
+        if (mark.applyPassport()) {
+            System.out.println("Congratulations " + mark.getName() + ". Your passport was approved! Your seat number is " + mark.getSeatNumber());
+        } else {
+            System.out.println("We are sorry " + mark.getName() + ". We cannot process your application.");
+        }
+
+
+
     }
 }
--- week-2/ObjectsPractice/src/Person.java
+++ week-2/ObjectsPractice/src/Person.java
@@ -1,13 +1,17 @@
+import java.util.Arrays;
+
 public class Person {
     private String name;
     private String nationality;
     private String dateOfBirth;
+    private String[] passport;
     private int seatNumber;
 
-    public Person(String name, String nationality, String dateOfBirth, int seatNumber) {
+    public Person(String name, String nationality, String dateOfBirth, String[] passport, int seatNumber) {
         this.name = name;
         this.nationality = nationality;
         this.dateOfBirth = dateOfBirth;
+        this.passport = Arrays.copyOf(passport, passport.length);
         this.seatNumber = seatNumber;
     }
 
@@ -23,7 +27,40 @@ public class Person {
         return dateOfBirth;
     }
 
+    public String[] getPassport() {
+        return Arrays.copyOf(this.passport, this.passport.length);
+    }
+
     public int getSeatNumber() {
         return seatNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setPassport(String[] passport) {
+        this.passport = Arrays.copyOf(passport, passport.length);
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public boolean applyPassport() {
+        int number = (int)(Math.random() * 2);
+        return number == 1;
+    }
+
+    public void chooseSeat() {
+        this.seatNumber = (int) (Math.random() * 11 + 1);
     }
 }
