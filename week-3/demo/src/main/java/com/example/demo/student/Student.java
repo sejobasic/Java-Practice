f77e21f90f801d57e1cab002c871b8eb68ee3b53
Sejo Basic <sejoob@gmail.com>
2022-12-21 15:46:45 -0700

    Created endpoint for student

--- week-3/demo/src/main/java/com/example/demo/student/Student.java
+++ week-3/demo/src/main/java/com/example/demo/student/Student.java
@@ -2,6 +2,7 @@ package com.example.demo.student;
 
 import java.time.LocalDate;
 
+
 public class Student {
 
     private Long id;
@@ -32,6 +33,46 @@ public class Student {
         this.name = name;
         this.email = email;
         this.dob = dob;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
         this.age = age;
     }
 }
