2c0d86fc56b8fbd8b71fcfd613a58dbca87cdd0c
Sejo Basic <sejoob@gmail.com>
2022-12-21 16:49:54 -0700

    Implemented POST and DELETE methods on student - tested in Postman

--- week-3/demo/src/main/java/com/example/demo/student/Student.java
+++ week-3/demo/src/main/java/com/example/demo/student/Student.java
@@ -86,4 +86,15 @@ public class Student {
     public void setAge(Integer age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", dob=" + dob +
+                ", age=" + age +
+                '}';
+    }
 }
