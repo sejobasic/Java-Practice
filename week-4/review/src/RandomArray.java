6d4518a06ccab4c5ccf25b2afbb77164bab824c1
Sejo Basic <sejoob@gmail.com>
2023-01-02 13:26:25 -0700

    Reverse array order

--- week-4/review/src/RandomArray.java
+++ week-4/review/src/RandomArray.java
@@ -11,19 +11,39 @@ public class RandomArray {
         int[] fourthArr = new int[10];
         int[] fifthArr = getRandomArray(10);
         int[] sixthArr = Arrays.copyOf(fifthArr, fifthArr.length);
+        int[] seventhArr = getRandomArray(10);
+
+        int[] s1 = {1, 2, 3, 4, 5};
+        int[] s2 = {1, 2, 3, 4, 5};
+
+        String[] stringArr = {"bob", "mark", "tim", "kyle"};
 
         Arrays.fill(fourthArr, 5);
-
         Arrays.sort(firstArr);
         Arrays.sort(secArr);
         Arrays.sort(thirdArr);
+        Arrays.sort(stringArr);
+        Arrays.sort(seventhArr);
 
-        System.out.println(Arrays.toString(firstArr));
-        System.out.println(Arrays.toString(secArr));
-        System.out.println(Arrays.toString(thirdArr));
-        System.out.println(Arrays.toString(fourthArr));
-        System.out.println(Arrays.toString(fifthArr));
-        System.out.println(Arrays.toString(sixthArr));
+        int[] reversedSeventh = reverseArray(seventhArr, 10);
+
+
+//        System.out.println(Arrays.toString(firstArr));
+//        System.out.println(Arrays.toString(secArr));
+//        System.out.println(Arrays.toString(thirdArr));
+//        System.out.println(Arrays.toString(fourthArr));
+//        System.out.println(Arrays.toString(fifthArr));
+//        System.out.println(Arrays.toString(sixthArr));
+        System.out.println(Arrays.toString(seventhArr));
+        System.out.println(Arrays.toString(reversedSeventh));
+
+        if (Arrays.binarySearch(stringArr, "tim") >= 0) {
+            System.out.println("Found tim in the list");
+        }
+
+        if (Arrays.equals(s1, s2)) {
+            System.out.println("Arrays are equal");
+        }
 
 
     }
@@ -36,5 +56,15 @@ public class RandomArray {
             newIntArr[i] = random.nextInt(100);
         }
         return newIntArr;
+    }
+
+    private static int[] reverseArray(int[] arr, int length) {
+        int[] reversedArr = new int[length];
+        int temp = length;
+        for (int i = 0; i < length; i++) {
+            reversedArr[temp - 1] = arr[i];
+            temp = temp - 1;
+        }
+        return reversedArr;
     }
 }
